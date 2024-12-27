f1c03740db058e3ea13188bd5b51775ba454aae1
Sreekanth <sreekanthb87@gmail.com>
2024-12-27 09:37:48 +0100

    V24 : Linear search
    Linear Search in an Integer Array
    When to Use: Small datasets or unsorted arrays.
    Time Complexity: 𝑂(𝑛)

---
+++ DSPlayground/Searching/LinearSearch/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace LinearSearch;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int[] numbers = { 10, 20, 30, 40, 50 };
+        int target = 30;
+
+        int result = LinearSearch(numbers, target);
+
+        if (result != -1)
+            Console.WriteLine($"Element found at index {result}");
+        else
+            Console.WriteLine("Element not found");
+    }
+
+    static int LinearSearch(int[] array, int target)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] == target)
+                return i; // Return the index of the target element
+        }
+        return -1; // Return -1 if the element is not found
+    }
+}
