31bfe3875cd0aea3988ed4ac2976dc87e2574dca
Sreekanth <sreekanthb87@gmail.com>
2024-12-27 10:36:37 +0100

    V25: Linear search with array's variant
    Linear search with array's variant

---
+++ DSPlayground/Searching/LinearSearchArray/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace LinearSearchArray;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        string[] fruits = { "Apple", "Banana", "Cherry", "Date" };
+        string target = "Cherry";
+
+        int result = LinearSearch(fruits, target);
+
+        if (result != -1)
+            Console.WriteLine($"Element found at index {result}");
+        else
+            Console.WriteLine("Element not found");
+    }
+
+    static int LinearSearch<T>(T[] array, T target)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i].Equals(target))
+                return i; // Return the index of the target element
+        }
+        return -1; // Return -1 if the element is not found
+    }
+}
