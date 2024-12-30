cf537b4f0efeef7d64b6f992fa58d0c26a4304f6
Sreekanth <sreekanthb87@gmail.com>
2024-12-30 09:41:15 +0100

    V27: Linear search recursive
    When to Use: When a simple search in an array is needed,
    and recursion is a preferred or required approach (e.g., for learning or specific constraints).
    Time Complexity:
    Best Case: 𝑂(1) (if the element is at the start).
    Worst Case: 𝑂(𝑛) (if the element is not present or at the end).
    Space Complexity: 𝑂(𝑛)(due to recursion stack).

---
+++ DSPlayground/Searching/LinearSearchRecursive/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace LinearSearchRecursive;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int[] numbers = { 3, 8, 12, 17, 25 };
+        int target = 17;
+
+        int result = LinearSearchRecursive(numbers, target);
+
+        if (result != -1)
+            Console.WriteLine($"Element found at index {result}");
+        else
+            Console.WriteLine("Element not found");
+    }
+
+    static int LinearSearchRecursive(int[] array, int target, int index = 0)
+    {
+        if (index >= array.Length)
+            return -1; // Base case: not found
+        if (array[index] == target)
+            return index; // Base case: found
+        return LinearSearchRecursive(array, target, index + 1); // Recursive step
+    }
+}
