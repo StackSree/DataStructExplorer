d7cebd950c16f9bf5e853aefde9a131440db5df0
Sreekanth <sreekanthb87@gmail.com>
2025-01-02 08:57:59 +0100

    V34: SinglyLinked list
    A singly linked list has nodes with a value and a pointer to the next node only. This is a custom implementation since LinkedList<T> in C# is inherently doubly linked.

--- DSPlayground/LinkedList/SinglyLinkedList/Program.cs
+++ DSPlayground/LinkedList/SinglyLinkedList/Program.cs
@@ -4,6 +4,58 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        // Usage
+        var list = new SinglyLinkedList<int>();
+        list.Add(1);
+        list.Add(2);
+        list.Add(3);
+        list.Display();
     }
 }
+
+public class SinglyLinkedList<T>
+{
+    public class Node
+    {
+        public T Value;
+        public Node Next;
+
+        public Node(T value)
+        {
+            Value = value;
+            Next = null;
+        }
+    }
+
+    public Node Head;
+
+    public void Add(T value)
+    {
+        Node newNode = new Node(value);
+        if (Head == null)
+        {
+            Head = newNode;
+        }
+        else
+        {
+            Node current = Head;
+            while (current.Next != null)
+            {
+                current = current.Next;
+            }
+            current.Next = newNode;
+        }
+    }
+
+    public void Display()
+    {
+        Node current = Head;
+        while (current != null)
+        {
+            Console.Write(current.Value + " -> ");
+            current = current.Next;
+        }
+        Console.WriteLine("null");
+    }
+}
+
