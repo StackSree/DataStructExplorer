5f11b0aefe75018ba3e0beffc564ddd50b2c00aa
Sreekanth <sreekanthb87@gmail.com>
2025-01-02 08:53:15 +0100

    V33: Simple linked list implementation
    The LinkedList<T> class represents a doubly linked list in C#. It provides methods for adding, removing, and traversing elements.

---
+++ DSPlayground/LinkedList/CircularLinkedList/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace CircularLinkedList;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ DSPlayground/LinkedList/LinkedListDefault/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace LinkedListDefault;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        LinkedList<int> linkedList = new LinkedList<int>();
+
+        // Add elements
+        linkedList.AddLast(1);
+        linkedList.AddLast(2);
+        linkedList.AddLast(3);
+
+        // Insert an element at the beginning
+        linkedList.AddFirst(0);
+
+        // Remove an element
+        linkedList.Remove(2);
+
+        // Iterate through the list
+        foreach (var item in linkedList)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
