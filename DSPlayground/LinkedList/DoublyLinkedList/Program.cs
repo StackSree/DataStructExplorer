97dec537bc01ef6b830dbfc76e132f45867bf94b
Sreekanth <sreekanthb87@gmail.com>
2025-01-03 10:19:57 +0100

    V36: Doubly linked list
    C#'s LinkedList<T> is already a doubly linked list. here is the sample of custom doubly linked list.

--- DSPlayground/LinkedList/DoublyLinkedList/Program.cs
+++ DSPlayground/LinkedList/DoublyLinkedList/Program.cs
@@ -4,6 +4,69 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        // Usage
+        var list = new DoublyLinkedList<int>();
+        list.AddLast(1);
+        list.AddLast(2);
+        list.AddLast(3);
+        list.DisplayForward();
+        list.DisplayBackward();
+    }
+
+    public class DoublyLinkedList<T>
+    {
+        public class Node
+        {
+            public T Value;
+            public Node Next;
+            public Node Prev;
+
+            public Node(T value)
+            {
+                Value = value;
+                Next = null;
+                Prev = null;
+            }
+        }
+
+        public Node Head;
+        public Node Tail;
+
+        public void AddLast(T value)
+        {
+            Node newNode = new Node(value);
+            if (Head == null)
+            {
+                Head = Tail = newNode;
+            }
+            else
+            {
+                Tail.Next = newNode;
+                newNode.Prev = Tail;
+                Tail = newNode;
+            }
+        }
+
+        public void DisplayForward()
+        {
+            Node current = Head;
+            while (current != null)
+            {
+                Console.Write(current.Value + " -> ");
+                current = current.Next;
+            }
+            Console.WriteLine("null");
+        }
+
+        public void DisplayBackward()
+        {
+            Node current = Tail;
+            while (current != null)
+            {
+                Console.Write(current.Value + " -> ");
+                current = current.Prev;
+            }
+            Console.WriteLine("null");
+        }
     }
 }
