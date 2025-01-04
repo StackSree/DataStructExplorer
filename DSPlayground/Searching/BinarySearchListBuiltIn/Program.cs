b351f793a3d53efd95132fdba9ae5f933baf8c29
Sreekanth <sreekanthb87@gmail.com>
2025-01-04 13:23:16 +0100

    V41: Built-in List<T>.BinarySearch
    C# provides built-in binary search functionality using List<T>.BinarySearch()

---
+++ DSPlayground/Searching/BinarySearchListBuiltIn/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace BinarySearchListBuiltIn;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        List<int> list = new List<int> { 1, 3, 5, 7, 9, 11 };
+        int target = 7;
+
+        int result = list.BinarySearch(target);
+
+        Console.WriteLine(result >= 0 ? $"Found at index {result}" : "Not found");
+    }
+}
