77379a6910f6934be04a64d025f73f22f74e0264
Sreekanth <sreekanthb87@gmail.com>
2025-01-07 09:35:16 +0100

    V44: Stack class
    The Stack class in C# is a generic data structure that can store any type of elements, making it type-safe

---
+++ DSPlayground/Stacks/StackDefault/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace StackDefault;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Stack<int> stack = new Stack<int>();
+
+        // Push elements onto the stack
+        stack.Push(1);
+        stack.Push(2);
+        stack.Push(3);
+
+        // Peek the top element
+        Console.WriteLine("Top element: " + stack.Peek());
+
+        // Pop elements off the stack
+        Console.WriteLine("Popped: " + stack.Pop());
+        Console.WriteLine("Popped: " + stack.Pop());
+
+        // Check if the stack contains a value
+        Console.WriteLine("Contains 1: " + stack.Contains(1));
+
+        // Count of elements
+        Console.WriteLine("Count: " + stack.Count);
+    }
+}
