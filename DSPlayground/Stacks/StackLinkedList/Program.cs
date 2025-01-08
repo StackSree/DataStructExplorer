3ab4965c6f886040fb6cc1911433c97a14c56b24
Sreekanth <sreekanthb87@gmail.com>
2025-01-08 07:18:42 +0100

    V47: Stacks by linked list
    Instead of using an array or List<T>, a LinkedList<T> can be used for dynamic stack implementation.
    Advantages: Avoids resizing as elements grow.

---
+++ DSPlayground/Stacks/StackLinkedList/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace StackLinkedList;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var stack = new LinkedListStack<int>();
+        // Push elements onto the stack
+        stack.Push(1);
+        stack.Push(2);
+        stack.Push(3);
+
+        // Peek the top element
+        Console.WriteLine("Top element: " + stack.Peek());
+
+        // Pop elements off the stack
+        Console.WriteLine("Popped: " + stack.Pop());
+        Console.WriteLine("Popped: " + stack.Pop()); 
+        
+        Console.WriteLine("Elements: "+ stack.Count());
+    }
+}
+class LinkedListStack<T>
+{
+    private LinkedList<T> list = new LinkedList<T>();
+
+    public void Push(T value)
+    {
+        list.AddLast(value);
+    }
+
+    public T Pop()
+    {
+        if (list.Count == 0)
+            throw new InvalidOperationException("Stack is empty");
+
+        T value = list.Last.Value;
+        list.RemoveLast();
+        return value;
+    }
+
+    public T Peek()
+    {
+        if (list.Count == 0)
+            throw new InvalidOperationException("Stack is empty");
+
+        return list.Last.Value;
+    }
+
+    public int Count() => list.Count;
+}
