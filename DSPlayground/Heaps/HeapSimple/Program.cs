93771966547aff3fb9f3fa9581d26bedb160f6d1
Sreekanth <sreekanthb87@gmail.com>
2025-02-03 10:11:44 +0100

    V93: Simple Heap Demo
    In C#, the heap is a memory region where reference-type objects (such as classes, arrays, and delegates) are stored dynamically. It is managed by the .NET garbage collector (GC), which automatically reclaims memory when objects are no longer needed.

---
+++ DSPlayground/Heaps/HeapSimple/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace HeapSimple;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        // "p1" and "p2" are stored on the stack, but the objects are stored on the heap
+        Person p1 = new("Alice", 25);
+        Person p2 = new("Bob", 30);
+
+        p1.Display();
+        p2.Display();
+
+        // Now eligible for garbage collection
+        // Objects without references become eligible for garbage collection.
+        p1 = null;
+        p2 = null;
+
+        // Force garbage collection
+        // Not recommended for general use
+        GC.Collect();
+
+        // Wait for destructor to run
+        GC.WaitForPendingFinalizers(); 
+
+        Console.WriteLine("Garbage collection completed.");
+    }
+}
+
+
+record Person
+{
+    public string Name;
+    public int Age;
+
+    public Person(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+
+    public void Display()
+    {
+        Console.WriteLine($"Name: {Name}, Age: {Age}");
+    }
+
+    ~Person() => Console.WriteLine("Object destroyed!");
+}
