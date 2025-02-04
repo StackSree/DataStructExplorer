ce56f67a366e99dab604342d11c1491ea3424841
Sreekanth <sreekanthb87@gmail.com>
2025-02-04 09:33:45 +0100

    V95: Object pool
    Instead of frequently allocating and deallocating objects on the heap, we can reuse them using an object pool.
    
    Benefits:
    
    Reduces GC pressure by reusing objects instead of creating new ones.
    Useful for game development, networking, and high-performance applications.

---
+++ DSPlayground/Heaps/HeapObjectPool/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace HeapObjectPool;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        ObjectPool<Sample> pool = new();
+
+        // Get an object from the pool
+        Sample obj1 = pool.GetObject();
+        obj1.Data = 42;
+
+        Console.WriteLine($"Object Data: {obj1.Data}");
+
+        // Return object to the pool
+        pool.ReturnObject(obj1);
+
+        // Get another object (reused)
+        Sample obj2 = pool.GetObject();
+        Console.WriteLine($"Reused Object Data: {obj2.Data}"); // Still 42
+    }
+}
+class Sample
+{
+    public int Data;
+}
+
+class ObjectPool<T> where T : new()
+{
+    private readonly Stack<T> _pool = new();
+
+    public T GetObject()
+    {
+        return _pool.Count > 0 ? _pool.Pop() : new T();
+    }
+
+    public void ReturnObject(T obj)
+    {
+        _pool.Push(obj);
+    }
+}
