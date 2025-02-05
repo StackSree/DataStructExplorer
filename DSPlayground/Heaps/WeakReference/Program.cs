24a488ee7ddd2b817641f85c74bf9c408092b9be
Sreekanth <sreekanthb87@gmail.com>
2025-02-05 08:34:01 +0100

    V97: Weak Reference
    Weak references allow an object to be collected by GC when needed, avoiding memory leaks.
    
    Prevents unnecessary memory retention.
    Useful for caching mechanisms.

---
+++ DSPlayground/Heaps/WeakReference/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace WeakReference;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        WeakReference<Sample> weakRef = new(new Sample { Name = "Temp Object" });
+
+        if (weakRef.TryGetTarget(out Sample obj))
+        {
+            Console.WriteLine($"Object is still alive: {obj.Name}");
+        }
+
+        GC.Collect(); // Force garbage collection
+
+        if (weakRef.TryGetTarget(out obj))
+        {
+            Console.WriteLine($"Object survived GC: {obj.Name}");
+        }
+        else
+        {
+            Console.WriteLine("Object was collected.");
+        }
+    }
+}
+
+
+class Sample
+{
+    public string Name;
+}
