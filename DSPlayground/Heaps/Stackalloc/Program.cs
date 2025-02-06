cc9bd322bd7a5095aac824ec68ebd83dc5e5aca0
Sreekanth <sreekanthb87@gmail.com>
2025-02-06 11:17:10 +0100

    V99: Stackalloc
    Use stackalloc for fixed-size arrays to allocate memory on the stack instead of the heap.
    
    Benefits:
    Avoids heap allocation for small fixed-size data.
    Reduces GC overhead.

---
+++ DSPlayground/Heaps/Stackalloc/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Stackalloc;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Span<int> numbers = stackalloc int[5] { 1, 2, 3, 4, 5 };
+
+        Console.WriteLine($"First Number: {numbers[0]}"); // Output: 1
+    }
+}
