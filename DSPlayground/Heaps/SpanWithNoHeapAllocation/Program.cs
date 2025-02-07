9e51c558340006ebfceb77e2551121c789a3f7e0
Sreekanth <sreekanthb87@gmail.com>
2025-02-07 09:21:43 +0100

    V102: Using stackalloc with Span<T> (Heap-Free Allocations)
     No heap allocation occurs here, making it extremely efficient. Useful for small, short-lived data structures.

---
+++ DSPlayground/Heaps/SpanWithNoHeapAllocation/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace SpanWithNoHeapAllocation;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Span<int> numbers = stackalloc int[3] { 1, 2, 3 };
+
+        numbers[0] = 100;
+        Console.WriteLine(numbers[0]); // Output: 100
+    }
+}
