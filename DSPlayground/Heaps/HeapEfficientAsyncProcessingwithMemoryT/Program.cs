9d5baa7c33aaf64941988a3090df797bf4e88158
Sreekanth <sreekanthb87@gmail.com>
2025-02-12 08:04:42 +0100

    V108: Span<T> can't be used in async methods, so we use Memory<T> instead.
     Why Use Memory<T> Here?
    
    Span<T> can't be used in async methods (stack-only).
    Memory<T> works in async scenarios and prevents heap fragmentation.

---
+++ DSPlayground/Heaps/HeapEfficientAsyncProcessingwithMemoryT/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace HeapEfficientAsyncProcessingwithMemoryT;
+
+internal class Program
+{
+    static async Task Main(string[] args)
+    {
+        byte[] buffer = new byte[1024];
+
+        string filePath = $"{AppDomain.CurrentDomain.BaseDirectory}log.txt";
+
+        using FileStream fs = new(filePath, FileMode.Open);
+        Memory<byte> memoryBuffer = buffer;
+
+        int bytesRead = await fs.ReadAsync(memoryBuffer); // No extra allocations!
+        Console.WriteLine($"Bytes Read: {bytesRead}");
+    }
+}
